8fc022b0462bfcc34b220507a4eb4f844f09e02d
ERROR244 <khalilsohail24@gmail.com>
2025-05-03 19:21:53 +0100

    ...

--- Makefile
+++ Makefile
@@ -1,22 +1,28 @@
 up:
-	sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
+	@sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
 	docker-compose -f ./srcs/docker-compose.yml up -d
 
 build:
-	sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
-	docker-compose -f ./srcs/docker-compose.yml up -d --build
+	@sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
+	@docker-compose -f ./srcs/docker-compose.yml up -d --build
 
 down:
-	docker-compose -f ./srcs/docker-compose.yml down -v
-	sudo rm -rf ./srcs/volumes/mariadb_data/* ./srcs/volumes/wordpress_data/*
+	@docker-compose -f ./srcs/docker-compose.yml down
+	@sudo rm -rf ./srcs/volumes/mariadb_data/* ./srcs/volumes/wordpress_data/*
 
 ps:
-	docker-compose -f ./srcs/docker-compose.yml ps
+	@docker-compose -f ./srcs/docker-compose.yml ps
 
 logs:
-	docker-compose -f ./srcs/docker-compose.yml logs
+	@docker-compose -f ./srcs/docker-compose.yml logs
 
 reUp: down
-	sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
-	docker-compose -f ./srcs/docker-compose.yml up -d
+	@sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
+	@docker-compose -f ./srcs/docker-compose.yml up -d
+	@clear
+
+reBuild: down
+	@sudo mkdir -p ./srcs/volumes/mariadb_data ./srcs/volumes/wordpress_data
+	@docker-compose -f ./srcs/docker-compose.yml up -d --build
+	@clear
 
---
+++ srcs/requirements/wordpress/tools/Script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+sleep 5
+
+# Set correct ownership and permissions
+chmod -R 755 /var/www/html
+chown -R www-data:www-data /var/www/html
+
+# Install WP-CLI if it's not already installed
+if ! command -v wp &> /dev/null; then
+  echo "Installing WP-CLI..."
+  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+    && chmod +x wp-cli.phar \
+    && mv wp-cli.phar /usr/local/bin/wp
+fi
+
+
+echo "Downloading WordPress..."
+curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+tar -xzf /tmp/wordpress.tar.gz -C /tmp
+cp -r /tmp/wordpress/* /var/www/html/
+cp -r /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+rm -rf /tmp/wordpress /tmp/wordpress.tar.gz
+
+sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" /var/www/html/wp-config.php
+sed -i "s/username_here/${WORDPRESS_DB_USER}/"      /var/www/html/wp-config.php
+sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/"  /var/www/html/wp-config.php
+sed -i "s/localhost/${WORDPRESS_DB_HOST}/"          /var/www/html/wp-config.php
+
+sed -i "s#listen = /run/php/php8.2-fpm.sock#listen = 0.0.0.0:9000#" /etc/php/8.2/fpm/pool.d/www.conf
+
+
+
+
+# echo "Configuring WordPress..."
+# wp config create \
+# --path=/var/www/html \
+# --dbname="${WORDPRESS_DB_NAME}" \
+# --dbuser="${WORDPRESS_DB_USER}" \
+# --dbpass="${WORDPRESS_DB_PASSWORD}" \
+# --dbhost="${WORDPRESS_DB_HOST}" \
+# --allow-root
+
+echo "Installing WordPress..."
+wp core install \
+    --path=/var/www/html \
+    --url="https://localhost" \
+    --title="Inception Site" \
+    --admin_user=admin \
+    --admin_password=admin \
+    --admin_email=admin@example.com \
+    --skip-email \
+    --allow-root
+
+echo "Starting PHP-FPM with version: ${PHP_VERSION:-8.2}"
+php-fpm${PHP_VERSION:-8.2} -F
